57e516930855e0fc17c6fb63552fbadb1ad3f3fe
Anrei Korotkov <akorotkov1497@gmail.com>
2020-10-21 10:55:24 +0200

    registration only for unique usernames, need to fetch response on the client side

--- src/main/java/com/example/tortiko/controller/UserController.java
+++ src/main/java/com/example/tortiko/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.tortiko.controller;
 
+import com.example.tortiko.model.ResponseTransfer;
 import com.example.tortiko.model.User;
 import com.example.tortiko.model.UserDTO;
 import com.example.tortiko.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.Random;
 import java.util.UUID;
 
 @Controller
@@ -29,13 +28,17 @@ public class UserController {
     }
 
     @RequestMapping(value = "api/users/create-user", method = RequestMethod.POST)
-    public String createUser(@RequestBody UserDTO user) {
-        User newUser = new User();
-        newUser.setUserId(UUID.randomUUID().toString());
-        newUser.setUsername(user.getUsername());
-        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(newUser);
-        return "redirect:/api/users/{userId}";
+    @ResponseBody
+    public ResponseTransfer createUser(@RequestBody UserDTO user) {
+        if(userRepository.findByUsername(user.getUsername()) != null){
+            return new ResponseTransfer("User already exist, please log in");
+        }
+            User newUser = new User();
+            newUser.setUserId(UUID.randomUUID().toString());
+            newUser.setUsername(user.getUsername());
+            newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(newUser);
+            return new ResponseTransfer("Successful registration!");
     }
 
     @DeleteMapping("api/users/{userId}")
