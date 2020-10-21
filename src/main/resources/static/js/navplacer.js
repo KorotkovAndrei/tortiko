c5defee8c94c76d0a84eac1a929aaab680aaa131
dreamvibe1993 <dreamvibe@mail.ru>
2020-10-21 20:26:26 +0500

    navbar placement function

---
+++ src/main/resources/static/js/navplacer.js
@@ -0,0 +1,40 @@
+function placeNavbar() {
+  let navbar = document.getElementById("nav-placeholder");
+  navbar.insertAdjacentHTML('afterbegin', `
+
+  <nav>
+      <div class="logo">
+          <h4>TORTIKO</h4>
+      </div>
+      <ul class="nav-links">
+          <li>
+              <a href="#">Home</a>
+          </li>
+          <li>
+              <a href="#">About</a>
+          </li>
+          <li class="shop">
+              <a  href="/shop.html">Shop</a>
+          </li>
+          <li>
+              <a href="#">Contact</a>
+          </li>
+          <li></li>
+          <li>
+              <a class="login-nav" id='lgnButton'>Login</a>
+          </li>
+          <li>
+              <a class="logout-nav hide" href='/logout' >Logout</a>
+          </li>
+      </ul>
+      <div class="burger">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+      </div>
+  </nav>
+
+  `)
+}
+
+window.onload(placeNavbar());
