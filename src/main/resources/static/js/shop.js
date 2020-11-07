4e421943472ea7925b2fcb141297b2b2beb165dc
dreamvibe1993 <dreamvibe@mail.ru>
2020-11-07 22:46:17 +0500

    product card is impemented

--- src/main/resources/static/js/shop.js
+++ src/main/resources/static/js/shop.js
@@ -32,3 +32,52 @@ function getProducts() {
         .then(response => response.json()).then(productList => updateProductsInDom(productList));
 }
 getProducts();
+
+let shopUI = document.querySelector('#shop-container-main');
+let shopDashboard = document.querySelector('#shop-container-secondary');
+let shopDashboardChildren = shopDashboard.childNodes;
+console.log(shopDashboardChildren);
+
+let c;
+shopUI.onclick = (event) => {
+  console.log("event.target.parentNode.className: " + event.target.parentNode.className + ",");
+  for (let i = 0; i < shopDashboardChildren.length; i++) {
+    if (shopDashboardChildren[i].className == event.target.className) {
+      console.log('shopDashboardChildren[i].className: ' + shopDashboardChildren[i].className)
+      c = 1;
+      return showtheprod(shopDashboardChildren[i].className);
+    }
+  }
+  if (c == 1) {
+    c = 0;
+    hidetheprod();
+  }
+}
+
+function showtheprod(classname) {
+  let cont = document.getElementById('prodcont');
+  cont.classList.toggle('off');
+  cont.classList.toggle('grid');
+  if (classname == "shop-container") {
+    console.log('classname one :' + classname);
+    let currelement = document.getElementsByClassName(classname);
+    console.log('currelement one :' + currelement);
+    let currelementheader = currelement.getElementsByTagName('h2');
+    contpname.innerHTML = currelementheader.innerHTML;
+  }
+  if (classname == "prodpic") {
+    console.log('classname two :' + classname);
+    classname = "shop-container";
+    let currelement = document.querySelector(`.${classname}`);
+        console.log('currelement two :' + currelement);
+    let currelementheader = currelement.querySelector('#shoppingcardname');
+    contpname.innerHTML = currelementheader.innerHTML;
+  }
+  console.log('showtheprod classname: ' + classname);
+}
+function hidetheprod() {
+  let cont = document.getElementById('prodcont');
+  cont.classList.toggle('off');
+  cont.classList.toggle('grid');
+  contpname.innerHTML = ''
+}
