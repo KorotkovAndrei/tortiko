e6b4abe045e789f1ae87b47a0844d0bf61f3f9fd
Anrei Korotkov <akorotkov1497@gmail.com>
2020-11-12 14:33:46 +0100

    OrderDTO implemented

---
+++ src/main/java/com/example/tortiko/dto/CustomerProductOrder.java
@@ -0,0 +1,31 @@
+package com.example.tortiko.dto;
+
+public class CustomerProductOrder {
+    private String productName;
+    private String size;
+    private Integer quantity;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/main/java/com/example/tortiko/dto/OrderDTO.java
+++ src/main/java/com/example/tortiko/dto/OrderDTO.java
@@ -1,12 +1,33 @@
 package com.example.tortiko.dto;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
 import java.util.ArrayList;
 
 public class OrderDTO {
     private String customerName;
-    private ArrayList<ArrayList<Integer>> productList;
+    private ArrayList<CustomerProductOrder> productList;
     private Integer totalPrice;
 
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public ArrayList<CustomerProductOrder> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<CustomerProductOrder> productList) {
+        this.productList = productList;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
