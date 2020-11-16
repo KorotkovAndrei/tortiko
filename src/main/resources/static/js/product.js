8cd8fb64b45b492d1cf0d2ee7056f34778e9fb20
dreamvibe1993 <dreamvibe@mail.ru>
2020-11-16 15:15:41 +0500

    cake's account counters and sizes

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,29 +1,109 @@
+let gridrowthree = document.querySelector('.grid-row-3');
+let t_one = document.querySelector('#uno');
+let t_two = document.querySelector('#dos');
+let t_three = document.querySelector('#tres');
+let p_small = t_one.innerText;
+let p_med = t_two.innerText;
+let p_large = t_three.innerText;
+t_one.id = '1';
+t_one.innerText = '£' + p_small;
+t_two.remove();
+t_three.remove();
+
 const counteradd = () => {
   let c = parseInt(imgnumber.innerHTML);
-  console.log(c);
   imgnumber.innerHTML = c + 1;
 }
+
 const counterdeduct = () => {
   let c = parseInt(imgnumber.innerHTML);
   c <= 1 ? imgnumber.innerHTML = 1 : imgnumber.innerHTML = c - 1;
 }
-imgplus.onclick = counteradd;
-imgminus.onclick = counterdeduct;
 
-
-const placethehtml = (value) => {
-  let nest = document.querySelector('#placetopaste');
-  let place = document.querySelector('.placeofinterest');
-  let ndata = "${product.getProductPrices()" + `[${value}]}`
-  place.remove();
-  nest.insertAdjacentHTML('afterbegin', `<p th:text="${ndata}" class='placeofinterest'></p>`)
+const gettheprightprice = (int) => {
+  let i;
+  if (int == 1) {
+    i = p_small;
+    return i;
+  }
+  if (int == 2) {
+    i = p_med;
+    return i;
+  }
+  if (int == 3) {
+    i = p_large;
+    return i;
+  }
 }
-prodcontbtnsml.addEventListener('click', () => {
-  placethehtml(0);
-});
-prodcontbtnmed.addEventListener('click', () => {
-  placethehtml(1);
-});
-prodcontbtnlrg.addEventListener('click', () => {
-  placethehtml(2);
-});
+
+const updatethepricedivide = (str, int) => {
+  let todelete = document.querySelector('.placeofinterest_1') || document.querySelector('.placeofinterest');
+  let i = gettheprightprice(int);
+  let x = str - parseInt(i);
+  if (x <= 0) {
+    placetopaste.insertAdjacentHTML('afterbegin', `
+    <p id='${int}' class='placeofinterest'>£${i}</p>
+    `)
+    todelete.remove();
+    return
+  }
+  placetopaste.insertAdjacentHTML('afterbegin', `
+  <p id='${int}' class='placeofinterest'>£${x}</p>
+  `)
+  todelete.remove();
+}
+
+const updatethepricemult = (str, int) => {
+  let todelete = document.querySelector('.placeofinterest_1') || document.querySelector('.placeofinterest');
+  let i = gettheprightprice(int);
+  todelete.remove();
+  let x = str + parseInt(i);
+  placetopaste.insertAdjacentHTML('afterbegin', `
+  <p id='${int}' class='placeofinterest'>£${x}</p>
+  `)
+}
+
+const updatetheprice = (str, int) => {
+  imgnumber.innerHTML = '1';
+  let todelete = document.querySelector('.placeofinterest_1') || document.querySelector('.placeofinterest');
+  todelete.remove();
+  placetopaste.insertAdjacentHTML('afterbegin', `
+  <p id='${int}' class='placeofinterest'>£${str}</p>
+  `)
+}
+
+const mutatethesubj = () => {
+  let subj = parseInt(document.querySelector('.placeofinterest').innerText.slice(1));
+  return subj;
+}
+
+const mutatetheobj = () => {
+  let obj = parseInt(document.querySelector('.placeofinterest').id);
+  return obj;
+}
+
+imgplus.onclick = () => {
+  counteradd();
+  updatethepricemult(mutatethesubj(), mutatetheobj());
+};
+
+imgminus.onclick = () => {
+  counterdeduct();
+  updatethepricedivide(mutatethesubj(), mutatetheobj());
+};
+
+
+gridrowthree.onclick = (event) => {
+  let todelete = document.querySelector('.placeofinterest') || document.querySelector('.placeofinterest_1');
+  if (placetopaste.childNodes) {
+    if (prodcontbtnsml_1.checked) {
+      updatetheprice(parseInt(p_small), 1);
+    }
+    if (prodcontbtnmed_2.checked) {
+      updatetheprice(parseInt(p_med), 2);
+    }
+    if (prodcontbtnlrg_3.checked) {
+      updatetheprice(parseInt(p_large), 3);
+    }
+  }
+}
